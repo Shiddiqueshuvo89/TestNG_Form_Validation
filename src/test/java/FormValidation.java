5074933d8b491c872224d3f1656abd311ec322b5
unknown <shiddiqueshuvo@gmail.com>
2023-08-18 01:44:31 +0600

    Class name updated

--- src/test/java/FormValidation.java
+++ src/test/java/FormValidation.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class VerifyTitle {
+public class FormValidation {
 	
 	WebDriver driver;
 	
